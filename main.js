50fb3a5bccdbc9f9ff9c463201d89dc90df397b8
CYNELEVEN <Gemcoltd2022@gmail.com>
2026-02-24 22:26:06 +0000

    Update main.js

--- main.js
+++ main.js
@@ -5,17 +5,43 @@ class PlayScene extends Phaser.Scene {
   constructor() { super("Play"); }
 
   create() {
+
+    // Title
     this.add.text(16, 16, "CYNIK Beta 1", {
       fontFamily: "monospace",
       fontSize: "18px",
       color: "#ffffff"
     });
 
-    this.add.text(16, 46, "If you can see this, it runs.", {
-      fontFamily: "monospace",
-      fontSize: "14px",
-      color: "#cbd5e1"
-    });
+    // Create player (8-bit square)
+    this.player = this.add.rectangle(WIDTH / 2, HEIGHT / 2, 20, 20, 0x7aa7ff);
+
+    this.physics.add.existing(this.player);
+    this.player.body.setCollideWorldBounds(true);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.speed = 200;
+  }
+
+  update() {
+    const body = this.player.body;
+
+    body.setVelocity(0);
+
+    if (this.cursors.left.isDown) {
+      body.setVelocityX(-this.speed);
+    }
+    else if (this.cursors.right.isDown) {
+      body.setVelocityX(this.speed);
+    }
+
+    if (this.cursors.up.isDown) {
+      body.setVelocityY(-this.speed);
+    }
+    else if (this.cursors.down.isDown) {
+      body.setVelocityY(this.speed);
+    }
   }
 }
 
@@ -24,6 +50,10 @@ new Phaser.Game({
   width: WIDTH,
   height: HEIGHT,
   backgroundColor: "#0f1a2b",
+  physics: {
+    default: "arcade",
+    arcade: { debug: false }
+  },
   scene: [PlayScene],
   pixelArt: true
 });
